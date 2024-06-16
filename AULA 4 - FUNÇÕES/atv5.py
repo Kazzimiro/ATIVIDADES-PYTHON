93bcdba94cbd8fed4b8fe880de47ec3e76068ac8
Artur Casimiro <162062740+Kazzimiro@users.noreply.github.com>
2024-06-16 12:00:06 -0300

    Create atv5.py

---
+++ AULA 4 - FUNÇÕES/atv5.py
@@ -0,0 +1,9 @@
+def multiplicacao_numeros(a, b):
+    return a * b
+
+# Exemplo de uso da função:
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = multiplicacao_numeros(num1, num2)
+print(f"A multiplicação de {num1} por {num2} é igual a {resultado}.")
