d78226244a68b53dad2ba139aabc043467159d8b
Artur Casimiro <162062740+Kazzimiro@users.noreply.github.com>
2024-06-16 13:07:41 -0300

    Create atv7.py

---
+++ AULA 5 - FUNÇÕES II/atv7.py
@@ -0,0 +1,6 @@
+def criar_lista_de_compras(*args):
+    return list(args)
+
+# Exemplo de uso da função:
+lista_compras = criar_lista_de_compras("Maçã", "Banana", "Leite", "Pão")
+print(lista_compras)
