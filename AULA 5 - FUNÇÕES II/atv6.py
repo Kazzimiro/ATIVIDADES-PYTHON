d5d1547a01831d02c201b3e133ae9d78ca9cbbe0
Artur Casimiro <162062740+Kazzimiro@users.noreply.github.com>
2024-06-16 13:05:52 -0300

    Update atv6.py

--- AULA 5 - FUNÇÕES II/atv6.py
+++ AULA 5 - FUNÇÕES II/atv6.py
@@ -1,1 +1,9 @@
+from functools import reduce
 
+def encontrar_maior_string(lista):
+    return reduce(lambda x, y: x if len(x) > len(y) else y, lista)
+
+# Exemplo de uso da função:
+strings = ["python", "java", "javascript", "ruby", "NodeJs"]
+maior_string = encontrar_maior_string(strings)
+print(f"A maior string na lista é: {maior_string}")
