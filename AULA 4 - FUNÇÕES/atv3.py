f046342b5395ae5fc34d05cdaceda532f7f9e02d
Artur Casimiro <162062740+Kazzimiro@users.noreply.github.com>
2024-06-16 11:58:40 -0300

    Create atv3.py

---
+++ AULA 4 - FUNÇÕES/atv3.py
@@ -0,0 +1,9 @@
+def soma_numeros(a, b):
+    return a + b
+
+# Exemplo de uso da função:
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = soma_numeros(num1, num2)
+print(f"A soma de {num1} e {num2} é igual a {resultado}.")
