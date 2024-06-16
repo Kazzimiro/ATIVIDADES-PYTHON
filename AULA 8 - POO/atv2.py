fdda026942803e581a0aabc32f45e6280391450d
Artur Casimiro <162062740+Kazzimiro@users.noreply.github.com>
2024-06-16 18:44:53 -0300

    Create atv2.py

---
+++ AULA 8 - POO/atv2.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    def __init__(self, nome, idade, peso, genero):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.genero = genero
+
+# Exemplo de uso:
+pessoa1 = Pessoa(nome="Maria", idade=30, peso=65, genero="Feminino")
+print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}, Peso: {pessoa1.peso}, Gênero: {pessoa1.genero}")
