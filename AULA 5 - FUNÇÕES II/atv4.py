dbd54869eb79c000e89fab35375a8d2cd9dcfb43
Artur Casimiro <162062740+Kazzimiro@users.noreply.github.com>
2024-06-16 13:03:25 -0300

    Create atv4.py

---
+++ AULA 5 - FUNÇÕES II/atv4.py
@@ -0,0 +1,7 @@
+def dobrar_numeros(lista):
+    return list(map(lambda x: x * 2, lista))
+
+# Exemplo de uso da função:
+numeros = [1, 2, 3, 4, 5]
+resultado = dobrar_numeros(numeros)
+print(resultado)
